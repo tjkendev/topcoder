8836750bb01cb791d707785af507f5a613bf187d
jakejaga <jake-basu@hotmail.co.jp>
2017-05-21 04:39:26 +0900

    add comments

--- algorithm/contest/16929-TCO-2017-Round2A/easy.py
+++ algorithm/contest/16929-TCO-2017-Round2A/easy.py
@@ -1,3 +1,13 @@
+# encoding: utf-8
+# cherry🍒とstrawberry🍓がそれぞれc, s個ある。
+# Cielは以下の条件を満たすようにcake🎂を焼きたい。
+# - 全てのcherryとstrawberryを使い切る
+# - 焼くcakeの数は任意の正の整数
+# - 異なるcakeは異なる量のcherryとstrawberryを使う
+# - 各cakeは少なくとも一つのcherryとstrawberryを使う
+# - 各cakeに使うcherryとstrawberryの数は互いに素にならないようにする
+# 以上の条件を満たすcherryとstrawberryの分け方が存在する場合は"Possible"、
+# それ以外の場合は"Impossible"を出力せよ。
 class FoxAndCake2:
     def isPossible(self, c, s):
         p = "Possible"; q = "Impossible"
@@ -6,11 +16,14 @@ class FoxAndCake2:
             r = m % n
             return n if r == 0 else gcd(n, r)
         if gcd(c, s) > 1:
+            # (c, s)が互いに素でない => cake一個で満たせる
             return p
         if c % 2 == 1 and s % 2 == 1:
+            # (odd, odd) => (5, 5)以上であれば(3, 3) + (even, even)という分け方が存在
             if c >= 5 and s >= 5:
                 return p
         if c % 2 == 1 and s % 2 == 0:
+            # (odd, even) => (5, 8)以上であれば(3, 6) + (event, event)という分け方が存在
             if c >= 5 and s >= 8:
                 return p
         if c % 2 == 0 and s % 2 == 1:
--- algorithm/contest/16929-TCO-2017-Round2A/medium.py
+++ algorithm/contest/16929-TCO-2017-Round2A/medium.py
@@ -1,3 +1,9 @@
+# encoding: utf-8
+# あるn個の頂点集合があり、各辺の長さを1とした時の、
+# 頂点0と1からの各頂点iまでの最短距離dist0[i], dist1[i]が与えられる。
+# この条件を満たすグラフGが存在するかを判定し、存在するのであればその一つを出力せよ。
+# 出力: tuple(string)の隣接行列 ('Y': iとjの間に(長さ1の)辺が存在, 'N': otherwise)
+#     : 存在しない場合は空のtuple
 class DistanceZeroAndOne:
     def construct(self, dist0, dist1):
         if not dist0[0] == dist1[1] == 0 or not dist0[1] == dist1[0] > 0:
@@ -7,14 +13,18 @@ class DistanceZeroAndOne:
         for i in xrange(n):
             es[i][i] = 0
             for j in xrange(i+1, n):
+                # 頂点iと頂点jが隣接しているか計算
                 d0 = abs(dist0[i] - dist0[j])
                 d1 = abs(dist1[i] - dist1[j])
+                # (d0, d1) in [(0, 1), (1, 0), (1, 1)] => 頂点が隣接していると考えられる
                 if d0 + d1 == 1 or d0 == d1 == 1:
                     es[i][j] = es[j][i] = 1
+        # Warshall-Floyd Algorithm
         for k in xrange(n):
             for i in xrange(n):
                 for j in xrange(n):
                     es[i][j] = min(es[i][j], es[i][k] + es[k][j])
+        # 頂点0, 1から各頂点の距離までがdist0[i], dist1[i]と一致するかチェックする
         for i in xrange(n):
             if es[0][i] != dist0[i]:
                 return tuple()
